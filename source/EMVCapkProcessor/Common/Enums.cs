1ac1afa42720ec0fa1d5af0fada545bc57b0e37e
Jon <jon.bianco@trustcommerce.com>
2021-10-28 13:13:19 -0700

    Initial commit

---
+++ source/EMVCapkProcessor/Common/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace EMVCapkProcessor.Common
+{
+    public class Enums
+    {
+        public enum EMVFile
+        {
+            [StringValue("Attended_emv.dat")]
+            Attended = 0,
+            [StringValue("Unttended_emv.dat")] 
+            Unattended = 1
+        }
+    }
+}
---
+++ source/EMVCapkProcessor/Program.cs
@@ -0,0 +1,14 @@
+﻿using EMVCapkProcessor.Processor;
+using System.Threading.Tasks;
+using static EMVCapkProcessor.Common.Enums;
+
+namespace EMVCapkProcessor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CAPKProcessor.ProcessCapk(EMVFile.Attended);
+        }
+    }
+}
